8988c17833bf457cdeb340886d9fe8919d46ab0a
preslavb <preslavb@abv.bg>
2019-11-23 11:43:42 +0000

    Controllable Camera

--- Assets/Code/View/BoardCameraController.cs
+++ Assets/Code/View/BoardCameraController.cs
@@ -1,25 +1,35 @@
 using System;
 using Cinemachine;
 using UnityEngine;
+using _Extensions;
 
 namespace View
 {
     public class BoardCameraController:MonoBehaviour
     {
+        [SerializeField]
+        [Range(0.5f, 1.5f)]
+        private float _cameraDamping = 1;
+        
         private Vector3 _initialMousePosition;
         private Plane _planeToMoveOn;
         
         [SerializeField]
         private Camera _camera;
 
+        private CinemachineVirtualCamera _virtualCamera;
+
         private void Awake()
         {
             var board = ConstantAccess.Instance.Board;
             _planeToMoveOn = new Plane(board.transform.forward, board.transform.position);
+            _virtualCamera = GetComponent<CinemachineVirtualCamera>();
         }
 
         private void Update()
         {
+            transform.position = _camera.transform.position;
+            
             if (Input.GetMouseButtonDown(2))
             {
                 // Create a ray from the mousePosition
@@ -42,12 +52,21 @@ namespace View
                 {
                     var hitPoint = ray.GetPoint(distance);
 
-                    var difference = _camera.WorldToViewportPoint(hitPoint) - _camera.WorldToViewportPoint(_initialMousePosition);
+                    var difference = (_camera.WorldToViewportPoint(hitPoint) - _camera.WorldToViewportPoint(_initialMousePosition)) * distance * _cameraDamping;
+
+                    // Update the virtual camera's position
+                    transform.Translate(-difference.With(z: 0));
                     
-                    Debug.Log(difference);
-                    
-                    transform.Translate(difference);
+                    /*
+                     * This will prompt the actual scene camera to move as well, but that one is confined to the board space.
+                     * To fix this, we need to reset the virtual camera's position at the start of next frame
+                     */
                 }
+            }
+
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                transform.Translate(new Vector3().With(z: Input.mouseScrollDelta.y));
             }
         }
     }
--- Assets/Code/_Extensions/Extensions.cs
+++ Assets/Code/_Extensions/Extensions.cs
@@ -44,5 +44,10 @@ namespace _Extensions
             results = new GameObject[0];
             return false;
         }
+
+        public static Vector3 With(this Vector3 original, float? x = null, float? y = null, float? z = null)
+        {
+            return new Vector3(x ?? original.x, y ?? original.y, z ?? original.z);
+        }
     }
 }
