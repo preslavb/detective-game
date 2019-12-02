2fad4301e8373cc91b048583dfd953ae97f07b25
Preslavb <preslavb@abv.bg>
2019-12-02 15:11:45 +0000

    reimplement render and factory

--- Assets/Editor/CreateitemWindow/CreateItemWindowFactory.cs
+++ Assets/Editor/CreateitemWindow/CreateItemWindowFactory.cs
@@ -1,7 +1,23 @@
+using DesignData;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
 namespace Doozy.Editor.Nody.Windows
 {
-    public class CreateItemWindowFactory
+    public partial class NodyWindow
     {
-        
+        public partial class CreateItemWindow
+        {
+            private DesignDataScriptableObject CreateNewDesignDataObject()
+            {
+                var newDesignData = ScriptableObject.CreateInstance<DesignDataScriptableObject>();
+
+                AssetDatabase.CreateAsset(newDesignData, "Assets/DesignData/New Design Data.asset");
+                AssetDatabase.SaveAssets();
+
+                return newDesignData;
+            }
+        }
     }
 }
--- Assets/Editor/CreateitemWindow/CreateItemWindowRender.cs
+++ Assets/Editor/CreateitemWindow/CreateItemWindowRender.cs
@@ -1,4 +1,7 @@
 using DesignData;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Doozy.Editor.Nody.Windows
 {
@@ -6,9 +9,42 @@ namespace Doozy.Editor.Nody.Windows
     {
         public partial class CreateItemWindow
         {
-            public void LoadDesignData(DesignDataScriptableObject designData)
+            private void Rerender()
             {
-                _designData = designData;
+                // Clear the root element
+                rootVisualElement.Clear();
+                
+                // If there is no current item being edited, just show the create button
+                if (_designData == null)
+                {
+                    rootVisualElement.Add(new Button(() =>
+                    {
+                        _designData = CreateNewDesignDataObject();
+                        Rerender();
+                    })
+                    {
+                        text = "Create New Item"
+                    });
+                }
+                else
+                {
+                    // Create the inspector for the asset
+                    VisualElement designDataInspector = new InspectorElement(_designData);
+                    rootVisualElement.Add(designDataInspector);
+                    
+                    // Create the container for the nody window
+                    _nodyWindowContainer = new IMGUIContainer(() => _nodyWindowEmbedded.OnGUI());
+
+                    if (_designData.Test != null)
+                    {
+                        // Create and hook up the nody window
+                        _nodyWindowEmbedded = CreateInstance<NodyWindow>();
+                        _nodyWindowEmbedded.m_isEmbeddedWindow = true;
+                    }
+                    
+                    // Add all of the elements
+                    rootVisualElement.Add(_nodyWindowContainer);
+                }
             }
         }
     }
