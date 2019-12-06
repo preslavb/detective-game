e0c5d2e2c01cd88e2dd3b8a8d27d81ae17aa55cc
Preslavb <preslavb@abv.bg>
2019-12-06 11:20:42 +0000

    Splash screen:
    - Added a splash screen scene
    - Added the splash screen video to the scene
    - Scene transitions to game when the video ends
    - Added a still image from the beginning frame of the splash screen as a background to the unity splash screen for more coherence

---
+++ Assets/Code/View/SplashScreenController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class SplashScreenController : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer _videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _videoPlayer.loopPointReached += playableDirector => SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
